405339c78f8a0c80d824da5b1e324188e199d06e
Huỳnh Thanh Phúc <48717211+ThanhPhucHuynh@users.noreply.github.com>
2020-07-22 21:08:38 +0700

    new task for tomorrow

--- pushtask.bat
+++ pushtask.bat
@@ -1,6 +1,10 @@
 color b
 d:
 cd D:\Data\drive\CTU\task
+
+for /f %%i in ('dir "*.md" /b/a-d/od/t:c') do set LAST=%%i
+copy %LAST% README.md /y
+
 git add .
 git commit -m "new task for tomorrow"
 git push -u origin master
